30ac023ae1ac54d220d26a014ea4996a6df4f05a
Pavel Kolchev <pavelkolchev@gmail.com>
2019-04-08 00:02:00 +0300

    use rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-extern crate sha1;
 extern crate linked_hash_map;
+extern crate sha1;
 
 use std::env;
-use std::fs::File;
-use std::io::{self, BufReader, BufWriter, Error, ErrorKind,
-              Seek, SeekFrom, Read, Write};
-use std::path::Path;
 use std::ffi::OsStr;
-
-use sha1::Sha1;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Error, ErrorKind, Read, Seek,
+              SeekFrom, Write};
+use std::path::Path;
 
 use linked_hash_map::LinkedHashMap;
+use sha1::Sha1;
 
-const DATA_SIZE: usize = 1804;
-const HASH_SIZE: usize = 20;
-const SHA1_EMPTY: [u8; HASH_SIZE] = [218,  57,   163,  238,  94, 
-                                     107,  75,   13,   50,   85, 
-                                     191,  239,  149,  96,   24, 
-                                     144,  175,  216,  7,    9];
 const SUPPORTED_VERSIONS: [u32; 2] = [2, 3];
 const DEFAULT_CACHE_VERSION: u32 = 3;
+const DATA_SIZE: usize = 1804;
+const HASH_SIZE: usize = 20;
 const MAGIC_STRING: [u8; 4] = *b"DXVK";
+const SHA1_EMPTY: [u8; HASH_SIZE] = [
+    218, 57, 163, 238, 94, 107, 75, 13, 50, 85, 191, 239, 149, 96, 24, 144,
+    175, 216, 7, 9,
+];
 
 struct Config {
     output:  String,
@@ -50,7 +49,7 @@ struct DxvkStateCacheEntry {
 }
 
 impl DxvkStateCacheEntry {
-    fn new() -> DxvkStateCacheEntry {
+    fn new() -> Self {
         DxvkStateCacheEntry {
             data: [0; DATA_SIZE],
             hash: [0; HASH_SIZE]
@@ -108,40 +107,38 @@ impl DxvkStateCacheEntry {
     }
 }
 
+impl<R: Read> ReadEx for BufReader<R> {}
 trait ReadEx: Read {
     fn read_u32(&mut self) -> io::Result<u32> {
         let mut buf = [0; 4];
         match self.read(&mut buf) {
-            Ok(_) => {
-                Ok((u32::from(buf[0])      ) +
-                   (u32::from(buf[1]) <<  8) +
-                   (u32::from(buf[2]) << 16) +
-                   (u32::from(buf[3]) << 24))
-            },
+            Ok(_) => Ok((u32::from(buf[0]))
+                + (u32::from(buf[1]) << 8)
+                + (u32::from(buf[2]) << 16)
+                + (u32::from(buf[3]) << 24)),
             Err(e) => Err(e)
         }
     }
 }
-impl<R: Read> ReadEx for BufReader<R> {}
 
-trait WriteEx: Write { 
+impl<W: Write> WriteEx for BufWriter<W> {}
+trait WriteEx: Write {
     fn write_u32(&mut self, n: u32) -> io::Result<()> {
         let mut buf = [0; 4];
-        buf[0] =  n        as u8;
-        buf[1] = (n >> 8 ) as u8;
+        buf[0] = n as u8;
+        buf[1] = (n >> 8) as u8;
         buf[2] = (n >> 16) as u8;
         buf[3] = (n >> 24) as u8;
         self.write_all(&buf)
     }
 }
-impl<W: Write> WriteEx for BufWriter<W> { }
 
 fn print_help() {
     println!("Standalone dxvk-cache merger");
-    println!("USAGE:\n\tdxvk-cache-tool [OPTION]... [FILE]...\n");
+    println!("USAGE:\n\tdxvk-cache-tool [OPTION]... <FILE>...\n");
     println!("OPTIONS:");
     println!("\t-o, --output <FILE>\tOutput file");
-    println!("\t-t, --target [2,3]\tTarget version");
+    println!("\t-t, --target <2|3>\tTarget version");
 }
 
 fn process_args() -> Config {
@@ -154,15 +151,19 @@ fn process_args() -> Config {
                 std::process::exit(0);
             },
             "-o" | "--output" => {
-                config.output = args[i+1].to_owned();
-                args.drain(i..i+2);
+                config.output = args[i + 1].to_owned();
+                args.drain(i..i + 2);
             },
             "-t" | "--target" => {
-                config.version = args[i+1].parse().expect("Not a number");
-                args.drain(i..i+2);
+                config.version = args[i + 1].parse().expect("Not a number");
+                args.drain(i..i + 2);
             },
             _ => ()
         }
+    }
+    if args.len() <= 1 {
+        print_help();
+        std::process::exit(0);
     }
     args.remove(0);
     config.files = args;
@@ -173,8 +174,10 @@ fn main() -> Result<(), io::Error> {
     let config = process_args();
 
     if !SUPPORTED_VERSIONS.contains(&config.version) {
-        return Err(Error::new(ErrorKind::InvalidData,
-                format!("Unsupported target version {}", config.version)))
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            format!("Unsupported target version {}", config.version)
+        ));
     };
 
     let mut entries = LinkedHashMap::new();
@@ -183,20 +186,21 @@ fn main() -> Result<(), io::Error> {
         let path = Path::new(&file);
 
         if !path.exists() {
-            return Err(Error::new(ErrorKind::NotFound, 
-                "File does not exists"));
+            return Err(Error::new(ErrorKind::NotFound, "File does not exists"));
         }
 
         if path.extension().and_then(OsStr::to_str) != Some("dxvk-cache") {
-            return Err(Error::new(ErrorKind::InvalidInput, 
-                "File extension mismatch"));
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "File extension mismatch"
+            ));
         }
 
         let file = File::open(path).expect("Unable to open file");
         let mut reader = BufReader::new(file);
 
         let header = DxvkStateCacheHeader {
-            magic: {
+            magic:      {
                 let mut magic = [0; 4];
                 reader.read_exact(&mut magic)?;
                 magic
@@ -206,13 +210,17 @@ fn main() -> Result<(), io::Error> {
         };
 
         if header.magic != MAGIC_STRING {
-            return Err(Error::new(ErrorKind::InvalidData, 
-                "Magic string mismatch"));
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "Magic string mismatch"
+            ));
         }
 
         if !SUPPORTED_VERSIONS.contains(&header.version) {
-            return Err(Error::new(ErrorKind::InvalidData,
-                    format!("Unsupported cache version {}", header.version)))
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("Unsupported cache version {}", header.version)
+            ));
         };
 
         if header.version != config.version {
@@ -223,34 +231,36 @@ fn main() -> Result<(), io::Error> {
                 v if v < config.version => {
                     println!("Upgrading to version {}", config.version)
                 },
-                _   => ()
+                _ => ()
             }
         }
 
         assert!(header.entry_size == DATA_SIZE + HASH_SIZE);
-        
+
         loop {
             let mut entry = DxvkStateCacheEntry::new();
             match reader.read_exact(&mut entry.data) {
-                Ok(_)   =>  (),
-                Err(e)  =>  {
+                Ok(_) => (),
+                Err(e) => {
                     if e.kind() == io::ErrorKind::UnexpectedEof {
-                        break
+                        break;
                     }
                     return Err(e);
                 }
             };
             if config.version == header.version {
                 match reader.read_exact(&mut entry.hash) {
-                    Ok(_)   =>  (),
-                    Err(e)  =>  return Err(e)
+                    Ok(_) => (),
+                    Err(e) => return Err(e)
                 };
             } else {
                 match config.version {
                     3 => entry.upgrade_to_v3(),
                     2 => entry.downgrade_to_v2(),
-                    _ => panic!(format!("Unexpected cache version {}",
-                                    header.version))
+                    _ => panic!(format!(
+                        "Unexpected cache version {}",
+                        header.version
+                    ))
                 }
                 entry.hash = entry.compute_hash();
                 reader.seek(SeekFrom::Current(HASH_SIZE as i64))?;
@@ -262,8 +272,10 @@ fn main() -> Result<(), io::Error> {
     }
 
     if entries.is_empty() {
-        return Err(Error::new(ErrorKind::Other, 
-            "No valid cache entries found"));
+        return Err(Error::new(
+            ErrorKind::Other,
+            "No valid cache entries found"
+        ));
     }
 
     let file = File::create(&config.output)?;
@@ -276,8 +288,10 @@ fn main() -> Result<(), io::Error> {
         writer.write_all(hash)?;
     }
 
-    println!("Merged cache {} contains {} entries",
+    println!(
+        "Merged cache {} contains {} entries",
         &config.output,
-        entries.len());
+        entries.len()
+    );
     Ok(())
 }
